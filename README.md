4fd65667114db4cff7c9fe888f64fd64a6e33727
elizabec <elizabec@metropolia.fi>
2023-06-02 14:49:41 +0300

    Added database textfile and program file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # VALA_Library
 Coding Test for VALA
+
+Exercise 4: Library
+
+## How to run this program
+
+1. Clone the repository to your chosen machine using either HTTPS or SSH
+
+2. Navigate to the repository using your choice of Windows terminal (PowerShell or Command Prompt). This code has not been tested on Linux or iOS devices.
+
+3. Run the command 
+    '''
+    python library_db.py library.txt
+    '''
+
+4. The program menu will appear in the terminal window.
---
+++ library_db.py
@@ -0,0 +1,70 @@
+import sys
+
+def menu_select(menu):
+    while menu:
+        select = input("Please select from the following:\n1. Add a Book\n2. Show existing database\nQ. Exit\n")
+        if select == "1":
+            add_book()
+        elif select == "2":
+            show_database()
+        elif select == "3":
+            sort_database(sys.argv[1])
+        elif select == "Q" or select == "q":
+            print("Exiting...")
+            exit()
+        else:
+            print("Please choose a valid option")
+
+def add_book():
+    title = input("Please enter the book title: ")
+    author = input("Please enter the book author: ")
+    isbn = input("Please enter the ISBN number: ")
+    if len(isbn) < 13 or len(isbn) > 13:
+        print("The given ISBN number is not the correct length, please enter a valid 13-digit ISBN number")
+        isbn = input("Please enter the ISBN number: ")
+    year = input ("Please enter the publishing year: ")
+
+    print("\nYou have entered the following:\nTitle: " + title + "\nAuthor: " + author + "\nISBN: " + isbn + "\nPublished: " + year)
+    add = input("\nWould you like to add this to the database? (Y/N)\n")
+    if add == "Y" or add == "y":
+        print("Adding to database!")
+        app_file = title + "/" + author + "/" + isbn + "/" + year + "\n"
+        with open(sys.argv[1], "a") as lib:
+            lib.write(app_file)
+        sort_database(sys.argv[1])
+    elif add == "N" or add == "n":
+        print("Returning to menu...")
+
+def show_database():
+    with open(sys.argv[1], "r") as lib:
+        empty_check = lib.read(1) #ensuring that there is data to read in the database
+        if not empty_check:
+            print("There is nothing in the database!\n")
+    with open(sys.argv[1], "r") as lib:
+        for line in lib:
+            strip_lines=line.strip()
+            lib_line = strip_lines.split("/")
+            print("\nTitle: "+ lib_line[0])
+            print("Author: "+ lib_line[1])
+            print("ISBN: "+ lib_line[2])
+            print("Published: "+ lib_line[3])
+
+def sort_database(filepath):
+    temp_dict = {}
+    with open(filepath, "r") as lib:
+        for line in lib:
+            lines = line.strip()
+            dates = lines.split("/")
+            lib_key = dates[0] + "/" + dates[1] + "/" + dates[2] + "/"
+            lib_val = dates[3]
+            temp_dict[lib_key] = lib_val
+        temp = sorted(temp_dict.items(), key=lambda x:x[1])
+        lib_dict = dict(temp)
+    with open(filepath, "w") as lib:
+        for k, v in lib_dict.items():
+            sort_str = k + v + "\n"
+            lib.write(sort_str)
+
+if __name__ == '__main__':
+    menu_select(menu=True)
+    
